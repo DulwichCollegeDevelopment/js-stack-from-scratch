ebed9321d9136a6eca0666c9b36a0a36d3b6d6ef
HarzicN <harzicn@users.noreply.github.com>
2017-06-02 10:21:31 +0100

    start of project

---
+++ js/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by harzicn on 02/06/2017.
+ *
+ * Reference: https://github.com/verekia/js-stack-from-scratch/blob/85c0837e8a7d5b1db0356d81ff7d795e0b6b7da4/tutorial/01-node-yarn-package-json.md#readme
+ */
+console.log('Fancy meeting you here');
